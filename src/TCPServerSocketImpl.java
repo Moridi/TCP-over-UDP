56c89dbace124e9b53ecb6ab9887e07c30cb3aa3
moridi <m.moridi.2009@gmail.com>
2019-04-27 19:35:40 +0430

    Refactor accept

--- src/TCPServerSocketImpl.java
+++ src/TCPServerSocketImpl.java
@@ -13,14 +13,11 @@ public class TCPServerSocketImpl extends TCPServerSocket {
         this.socket = new EnhancedDatagramSocket(port);
     }
 
-    @Override
-    public TCPSocket accept() throws Exception {
-        short recvSeqNumber;
-
+    public DatagramPacket getSynPacket() throws Exception {
         byte buf[] = new byte[20];
         DatagramPacket dp = new DatagramPacket(buf, 20);
-
         TCPHeaderParser dpParser;
+        
         while (true) {
             socket.receive(dp);
             dpParser = new TCPHeaderParser(dp.getData());
@@ -28,8 +25,15 @@ public class TCPServerSocketImpl extends TCPServerSocket {
                 break;
         }
 
-        // TODO: save sender seq num
-        recvSeqNumber = dpParser.getSequenceNumber(); 
+        return dp;
+    }
+
+    public TCPHeaderGenerator setupSynAckPacket(DatagramPacket dp) throws Exception {
+
+        TCPHeaderParser dpParser = new TCPHeaderParser(dp.getData());
+
+        short recvSeqNumber = dpParser.getSequenceNumber(); 
+
         String hostAddress = dp.getAddress().getHostAddress();
         int hostPort = dp.getPort();
 
@@ -37,11 +41,14 @@ public class TCPServerSocketImpl extends TCPServerSocket {
         synAckPacket.setSynFlag();
         synAckPacket.setAckFlag();
         synAckPacket.setSequenceNumber((short)this.SequenceNum);
+
         // TODO: decide to whether user +1 or +palyload_size for AckNumber
         synAckPacket.setAckNumber((short)(recvSeqNumber + 1));
-        this.socket.send(synAckPacket.getPacket());
 
-        System.out.println("Sent");
+        return synAckPacket;
+    }
+
+    public void getAckPacket() throws Exception{
 
         byte ackBuf[] = new byte[20];
         DatagramPacket ackPacket = new DatagramPacket(ackBuf, 20);
@@ -53,11 +60,35 @@ public class TCPServerSocketImpl extends TCPServerSocket {
                     && (ackPacketParser.getAckNumber() == this.SequenceNum + 1))
                 break;
         }
+    }
+
+    public TCPSocketImpl createTcpSocket(DatagramPacket dp) {
+        String hostAddress = dp.getAddress().getHostAddress();
+        int hostPort = dp.getPort();
 
         System.out.println("Est");
         TCPSocketImpl tcpSocket = new TCPSocketImpl();
         tcpSocket.init(socket, hostAddress, hostPort);
+
         return tcpSocket;
+    } 
+    
+    @Override
+    public TCPSocket accept() throws Exception {
+
+        // Receive Syn Packet
+        DatagramPacket dp = getSynPacket();
+
+        // Send Syn/Ack Packet
+        TCPHeaderGenerator synAckPacket = setupSynAckPacket(dp);
+        this.socket.send(synAckPacket.getPacket());
+
+        System.out.println("Sent");
+
+        // Receive Ack packet
+        getAckPacket();
+
+        return createTcpSocket(dp);
     }
 
     @Override
