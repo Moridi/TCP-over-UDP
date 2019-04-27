30c3fb729ff809d55e814528094c8f9dcc7956a1
moridi <m.moridi.2009@gmail.com>
2019-04-27 15:34:07 +0430

    Add TCPHeaderGenerator class to the project

--- bin/run.sh
+++ bin/run.sh
@@ -1,1 +1,1 @@
-javac ../src/*.java
+javac -d . ../src/*.java
---
+++ src/TCPHeaderGenerator.java
@@ -0,0 +1,32 @@
+// package TCPHeaderGenerator;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+
+public class TCPHeaderGenerator {
+    private int MINIMUM_HEADER_SIZE = 40;
+
+    private byte buffer[];
+    private int port;
+    private String ip;
+
+    // [0] | [1] | [2] | [3] | [4] | [5] | [6]:ACK | [7]:SYN
+    public int flagIndex = 0;
+
+    public TCPHeaderGenerator(String _ip, int _port) {
+        this.port = _port;
+        this.ip = _ip;
+        this.buffer = new byte[MINIMUM_HEADER_SIZE];
+        buffer[flagIndex] = 0x00;
+    }
+
+    public void setSynFlag() {
+        byte setSyn = 0x01;
+        buffer[flagIndex] = (byte)(buffer[flagIndex] | setSyn);
+    }
+
+    public DatagramPacket getPacket() throws Exception {
+        InetAddress address = InetAddress.getByName(ip);
+        return new DatagramPacket(buffer, buffer.length, address, port);
+    }
+}
