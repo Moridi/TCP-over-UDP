edac119820b60c843f6ef6b90a21738b975fe57b
moridi <m.moridi.2009@gmail.com>
2019-04-27 17:44:28 +0430

    Add TCP Header Parser

---
+++ src/TCPHeaderParser.java
@@ -0,0 +1,18 @@
+
+public class TCPHeaderParser {
+    private byte buffer[];
+    private int MINIMUM_HEADER_SIZE = 40;
+
+    // [0] | [1] | [2] | [3] | [4] | [5] | [6]:ACK | [7]:SYN
+    public int flagIndex = 0;
+
+    public TCPHeaderParser(byte buffer[]) {
+        this.buffer = buffer;
+    }
+
+    public boolean isSynPacket() {
+        if ((buffer[flagIndex] & 0x01) == 0x01)
+            return true;
+        return false;
+    }
+} 
